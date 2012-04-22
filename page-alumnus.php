a2fbfc7e248429fa431fc830817eb36062543e66
Daniel Slavov <daniel@avcora.com>
2012-04-22 19:49:27 +0300

    Template for alumnus page

---
+++ page-alumnus.php
@@ -0,0 +1,31 @@
+<?php 
+/*
+Template Name: Alumnus Page
+*/
+?>
+
+<?php get_header(); ?>
+
+  <?php 
+	$subpages = get_subpages();
+
+	$featured_alumnus = array();
+	
+	foreach ($subpages as $subpage) {
+		if (get_field("is_featured", $subpage->ID)) {
+			$featured_alumnus[] = $subpage;
+		}
+	}
+      
+	if (!$featured_alumnus) $featured_alumnus = $subpages;
+	
+	$alumni = $featured_alumnus[rand(0, sizeof($featured_alumnus) - 1)];	
+			
+    echo "<table>";
+	echo "<tr><td>" . $alumni->post_title . "</td></tr>";
+	echo "<tr><td>" .  wp_get_attachment_link(get_field("image", $alumni->ID), "thumbnail") . "</td></tr>"; 
+	echo "</tr><td><p>" . $alumni->post_content 			."</p></td></tr>";      	
+    echo "</table>";
+      
+  ?>
+<?php get_footer(); ?>
